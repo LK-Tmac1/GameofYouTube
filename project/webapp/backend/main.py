f5efb963f182db1506482b85b424592d57dcd1f5
liukun1016 <liukun1016@gmail.com>
2017-05-29 20:53:06 -0700

    command

--- project/webapp/backend/main.py
+++ project/webapp/backend/main.py
@@ -3,14 +3,19 @@ from utility import parse_main_arguments
 import sys
 
 
-if __name__ == '__main__':
-    arguments = sys.argv
-    arguments.append("--config_path=")
-    arguments.append("--channelId=kun")
-    arguments.append("--mode=channel_video")
-    arguments.append("--videos=1,2,3,4,5")
-    arguments.append("--activity=like")
+def test():
+    sys.argv.append("--config_path=")
+    sys.argv.append("--channelId=kun")
+    sys.argv.append("--mode=channel_video")
+    sys.argv.append("--videos=1,2,3,4,5")
+    sys.argv.append("--activity=like")
     # arguments.append("--tempo=0.01")
+    config = parse_main_arguments(sys.argv)
+    producer = ActivityProducer.build_producer(config=config)
+    producer.produce()
+
+
+if __name__ == '__main__':
     config = parse_main_arguments(sys.argv)
     producer = ActivityProducer.build_producer(config=config)
     producer.produce()
--- project/webapp/run.py
+++ project/webapp/run.py
@@ -3,10 +3,11 @@ from flask.globals import request
 from backend.youtube import SearchClient
 from backend.entity import Video, Channel
 from backend.utility import parse_main_arguments, read_file
-import sys
+import sys, subprocess
 
 app = Flask(__name__)
-config = {}
+config_path = None
+search_client = None
 
 
 @app.route('/video')
@@ -14,20 +15,38 @@ def video():
     return render_template("video.html", title='Video')
 
 
+@app.route('/video_stream')
+def video_stream():
+    return render_template("video.html", title='Video')
+
+
 @app.route('/video', methods=['POST'])
+@app.route('/video_stream', methods=['POST'])
 def video_search():
-    keyword = request.form.get("video_keyword")
-    json_data = search_client.search_video_by_keyword(keyword=keyword, part='snippet')
-    video_list = Video.parse_video_json(json_data=json_data)
-    return render_template("video.html", video_list=video_list)
+    if "video_keyword" in request.form:
+        keyword = request.form.get("video_keyword")
+        json_data = search_client.search_video_by_keyword(keyword=keyword, part='snippet')
+        video_list = Video.parse_video_json(json_data=json_data)
+        return render_template("video.html", video_list=video_list)
+    elif "video_info" in request.form:
+        video_info = request.form.getlist("video_info")[0]
+        video_id = video_info[0:video_info.rfind(':')]
+        video_title = video_info[video_info.rfind(':') + 1:len(video_info)]
+        activity = str(request.form["activity"])
+        cmd = "python ./main.py --mode=video --videos=%s --activity=%s --config_path=%s" % (video_id, activity, config_path)
+        cmd = "spark-submit --class abc --kafka-topic=%s --runsum=false --seconds=10 --config_path=%s <path to jar file>"
+        subprocess.Popen(cmd.split(" "))
+        return render_template("video.html")
 
 
 @app.route('/channel')
+@app.route('/channel_stream')
 def channel():
     return render_template("channel.html", title="Channel")
 
 
 @app.route('/channel', methods=['POST'])
+@app.route('/channel_stream', methods=['POST'])
 def channel_search():
     keyword = request.form.get("channel_keyword")
     json_data = search_client.search_channel_by_keyword(keyword=keyword, part="snippet")
@@ -36,7 +55,8 @@ def channel_search():
 
 
 if __name__ == '__main__':
-    sys.argv.append("--config_path=/Users/kunliu/git/youtubetrends/project/config.yml")
+    arguments = sys.argv
+    arguments.append("--config_path=/Users/kunliu/git/youtubetrends/project/config.yml")
     if len(sys.argv) <= 1:
         print "Need to specify --config_path=<path to yaml config file>"
         exit(1)
